2e3c086eb88616742b3c7deda72530eebc66e3ff
ethereal_tea <anthonysedov72@gmail.com>
2024-07-13 16:16:40 +0300

    Add menu in mobile version

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello")
+const btn = document.querySelector(".header__menu-btn")
+const navigation = document.querySelector(".header__nav")
+
+btn.addEventListener("click", () => {
+    navigation.classList.toggle("header__nav--visible");
+    btn.classList.toggle("header__menu-btn--cross");
+})
